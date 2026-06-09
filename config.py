f80e84c3ade34185bd2e2017a5d6cff94c1578f4
Truongvanhuyhuy <103748116+Truongvanhuyhuy@users.noreply.github.com>
2026-06-09 11:19:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 123456  # api id
-API_HASH = "ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # api hash
+API_ID = 35935694  # api id
+API_HASH = "270fc7395c68e32a907b0baaf910700c"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "8678869575:AAGgF-LGcw0UP3d6YB3D7K3EaIqBcS5NhDs"  # bot token
 
 
 # REDIS
@@ -9,13 +9,13 @@ HOST = "localhost"  # redis host uri
 PORT = 6379  # redis port
 PASSWORD = ""  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1003998476130  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
 COOKIE = ""
-ADMINS = [1317173146]
+ADMINS = [5575707907]
 
 
-BOT_USERNAME = "teraboxdown_bot"
+BOT_USERNAME = "phuoc_download_bot"
 
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
 FORCE_LINK = "@RoldexVerse"
